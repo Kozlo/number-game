b1743646a9491161868b8db4d6d291b9e40e940a
Martins Kozlovskis <mkozlovskis@gmail.com>
2017-02-28 18:19:38 +0200

    added basic files and an express server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
+// dependencies
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// components
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+// services
+import { PostsService } from './posts.service';
+
+// Define the routes
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES) // Add routes to the app
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
